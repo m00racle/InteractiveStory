6f595250621d21f32190fd1712a91da15d17e6bf
mooracle <yanuar.prakosa@gmail.com>
2018-10-11 09:58:24 +0700

    Entry 15: Organizing MVP pattern directories

--- app/src/main/java/com/mooracle/interactivestory/ui/StoryActivity.java
+++ app/src/main/java/com/mooracle/interactivestory/ui/StoryActivity.java
@@ -1,9 +1,10 @@
-package com.mooracle.interactivestory;
+package com.mooracle.interactivestory.ui;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import com.mooracle.interactivestory.R;
 
 public class StoryActivity extends AppCompatActivity {
 
